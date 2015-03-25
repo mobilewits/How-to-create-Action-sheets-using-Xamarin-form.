a35d88ee92f50b1d0fa9aa06fac2aff8ffb4a606
Harry <Harry@Harrys-Mac-Pro-2.local>
2015-03-25 11:22:55 -0400

    How to create Action sheets using Xamarin form.

---
+++ ActionSheetXamrineForm/ActionSheetPage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ActionSheetXamrineForm
+{
+	public class ActionSheetPage : ContentPage
+	{
+
+		public ActionSheetPage ()
+		{
+
+			var label = new Label
+			{
+				Text = "ActionSheetDemo",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand, 
+			};
+			var btn1 = new Button
+			{ 
+				Text="ActionSheet Simple ",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand, 
+			};
+			var btn2 = new Button
+			{ 
+				Text="ActionSheet Cancel/Delete",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand, 
+			};
+
+			btn1.Clicked += async (sender, e) => {
+				var action = await DisplayActionSheet ("ActionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+
+			};
+			btn2.Clicked += async (sender, e) => {
+				var action= await DisplayActionSheet ("ActionSheet: Save Photo?","Cancel","Delete","Photo Roll","Email");
+			};
+
+			Content = new StackLayout {
+				Children = {
+
+					label,btn1,btn2
+				}
+			};
+		}
+	}
+}
+
+
---
+++ ActionSheetXamrineForm/ActionSheetXamrineForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ActionSheetXamrineForm
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			MainPage = new NavigationPage (new ActionSheetPage ()){
+				Title="ActionSheet"
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
